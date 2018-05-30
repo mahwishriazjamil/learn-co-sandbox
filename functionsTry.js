3bc4fe1644d71ef21530a3ca2823df67e7047883
Mahwish Riaz-Jamil <mahwishcee@gmail.com>
2018-05-30 21:25:38 +0000

    Done.

---
+++ functionsTry.js
@@ -0,0 +1,26 @@
+function sayHello(){
+  console.log('Hello!')
+}
+
+function sayHelloIsabel(){
+  console.log('Hello, Isabel!')
+}
+
+function sayHelloSofia(){
+  console.log('Hello, Sofia!')
+}
+
+function sayHelloBrendan(){
+  console.log('Hello, Brendan!')
+}
+
+function sayHelloTo(greeting, firstName){
+  return `${greeting}, ${firstName}!`
+}
+
+function add(x, y){
+  console.log(x+y)
+}
+
+add(80, 9000)
+sayHelloTo("Goodbye", "Julio")
---
+++ varTry.js
@@ -0,0 +1,15 @@
+var word = 'bird'
+
+return word
+
+word = 'dog'
+
+return word
+
+var a = 5,
+    b = 2,
+    c = 3,
+    d = 'hello',
+    e = 'goodbye';
+    
+    console.log(`${a}`, `${b}`, `${c}`,`${d}`,`${e}`)
